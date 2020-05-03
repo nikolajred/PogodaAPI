f56c8d764db44fcbf23a4cf7d9060e6c95c29cb4
nikolajred <nikolajred@gmail.com>
2020-05-03 23:24:03 +0300

    third commit

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -1,6 +1,7 @@
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
@@ -9,22 +10,20 @@ import java.net.URL;
 public class Parser {
     private static Document getPage() throws IOException {
         String url = "https://meteo.ua/150/harkov/";
-        Document page = Jsoup.parse(new URL(url), 3000);
+        Document page = Jsoup.parse(new URL(url), 5000);
         return page;
 
     }
 
     public static void main(String[] args) throws IOException {
         String date = "";
-        System.out.println(date + " t°C      Ощущается как   Вероятн.осадков     Давление, мм рт.ст.     Влажность, %    Ветер, м/с");
+        System.out.println(date + "  t°C      Ощущается как   Вероятн.осадков     Давление, мм рт.ст.     Влажность, %    Ветер, м/с");
 
         Document page = getPage();
-        Element contentWth = page.select("a[class = wwt_cont]").first();
-        Element tableWth = page.select("div[class = weather_info]").first();
-        System.out.println(contentWth);
-        System.out.println(tableWth);
-
-
+        Element allWth = page.select("div[class=week_weather]").first();
+        Elements names = allWth.select("a[class = wwt_cont]");
+        System.out.println(names);
+        //System.out.println(allWth);
     }
 
 }
